a2d58625327646d0bb2d11bdf21676665e7f801b
matdave <mat@matdave.com>
2022-11-29 15:13:55 -0600

    adding GPM files

---
+++ _build/gpm_scripts/gpm.script.tables.php
@@ -0,0 +1,27 @@
+<?php
+use xPDO\Transport\xPDOTransport;
+
+/**
+ * Create tables
+ *
+ * THIS SCRIPT IS AUTOMATICALLY GENERATED, NO CHANGES WILL APPLY
+ *
+ * @package linkstrategy
+ * @subpackage build.scripts
+ *
+ * @var \xPDO\Transport\xPDOTransport $transport
+ * @var array $object
+ * @var array $options
+ */
+
+$modx =& $transport->xpdo;
+
+if ($options[xPDOTransport::PACKAGE_ACTION] === xPDOTransport::ACTION_UNINSTALL) return true;
+
+$manager = $modx->getManager();
+
+$manager->createObjectContainer(LinkStrategy\Model\Links::class);
+$manager->createObjectContainer(LinkStrategy\Model\ResourceLinks::class);
+$manager->createObjectContainer(LinkStrategy\Model\ResourceLinksText::class);
+
+return true;
